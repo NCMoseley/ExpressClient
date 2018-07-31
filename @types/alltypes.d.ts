163150f7fce68d2ecd01afefd41ca6ccf3846e43
NCMoseley <ncmoseley@gmail.com>
2018-07-31 10:58:49 -0700

    Add routes

---
+++ @types/alltypes.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-router-dom';
+declare module 'react-router';
+declare module 'redux';
+declare module 'redux-form';
+declare module 'axios';
+declare module 'redux-thunk';
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import App from './components/App';
+import Welcome from './components/Welcome';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+  <BrowserRouter>
+    <App>
+      <Route path="/" exact={true} component={Welcome} />
+    </App>
+  </BrowserRouter>,
+  document.querySelector('#root')
 );
-registerServiceWorker();
